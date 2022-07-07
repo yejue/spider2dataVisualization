c28565e6453845a88451d85318254427e3690107
yejue <yejue@yjstudy1.com>
2022-07-07 12:09:01 +0800

    [update] 清除基础数据表模型 remarks 字段

--- libs/models.py
+++ libs/models.py
@@ -6,7 +6,6 @@ class BaseModel(models.Model):
 
     create_time = models.DateTimeField("创建时间", auto_now_add=True, help_text="创建时间")
     update_time = models.DateTimeField("更新时间", auto_now=True, help_text="更新时间")
-    remarks = models.CharField("备注", max_length=128, null=True)
 
     class Meta:
         abstract = True
