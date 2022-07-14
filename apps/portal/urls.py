626b150f752db2271409c5bd4ac7c7190acbb172
yejue <yejue@yjstudy1.com>
2022-07-14 22:30:45 +0800

    [add] 添加了首页图标；大屏视图和视频列表视图，以及对应的路由和临时页面

--- apps/portal/urls.py
+++ apps/portal/urls.py
@@ -5,5 +5,7 @@ app_name = "portal"
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
-    path("distribution/", views.IndexView.as_view(), name="distribution"),
+    path("distribution/", views.HousingPriceDistributionView.as_view(), name="distribution"),
+    path("big-view/", views.BigViewView.as_view(), name="big_view"),
+    path("video-list/", views.VideoListView.as_view(), name="video_list"),
 ]
--- apps/portal/views.py
+++ apps/portal/views.py
@@ -13,3 +13,15 @@ class HousingPriceDistributionView(View):
     """房价分布页视图"""
     def get(self, request):
         return render(self.request, "portal/distribution.html")
+
+
+class BigViewView(View):
+    """大屏图表视图"""
+    def get(self, request):
+        return render(self.request, "portal/big_view.html")
+
+
+class VideoListView(View):
+    """可视化图表赛跑视频列表视图"""
+    def get(self, request):
+        return render(self.request, "portal/video_list.html")
