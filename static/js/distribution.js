38bc9ed81171d5b6f8c8f7143886b4b3cc57e609
zyx_link <2788998580@qq.com>
2022-07-18 11:12:02 +0800

    [update] js 代码规范化，写上注释.

--- static/js/distribution.js
+++ static/js/distribution.js
@@ -1,16 +1,23 @@
 const heat_map_data = document.getElementById('heatmap-data');
-const heat_map_data_obj = JSON.parse(heat_map_data.innerText);
-var heat_map_data_json = new Array();
-heat_map_data_json.push(heat_map_data_obj);
-const dom = document.getElementById('container');
+
+const heat_map_data_obj = JSON.parse(heat_map_data.innerText); // 以上两步是获取后端渲染来的数据，并将其转换为 json 格式数据。
+
+var heat_map_data_json = [];
+
+heat_map_data_json.push(heat_map_data_obj); // heatmap 需要的数据格式是 [ json ] 因此在这里创建了一个列表
+
 const top10_data = document.getElementById('top10-data');
-const top10_data_json = JSON.parse(top10_data.innerText);
+
+const top10_data_json = JSON.parse(top10_data.innerText); // 参考上面的例子，获取总价/单价前十的 json 数据。
+
+const dom = document.getElementById('container');
 
 var myChart = echarts.init(dom, null, {
     renderer: 'canvas',
     useDirtyRect: false
-});
-var option;
+}); // 初始化一个 echarts 实例，准备绘制热力图.
+
+var option; // 设置项变量
 
 const address_list = [
     {
@@ -25,7 +32,8 @@ const address_list = [
         '东海花园': [114.02818824989453, 22.544343201333714],
         '鲸山觐海': [113.91373496524493, 22.48470556452431],
     },
-]
+] // 房价总价前十的小区的定位。
+
 const total_price = [
     {name: '观澜湖', value: 8500},
     {name: '西丽山庄', value: 6838},
@@ -37,8 +45,9 @@ const total_price = [
     {name: '波托菲诺纯水岸十五期', value: 3856.0},
     {name: '东海花园', value: 3823.0},
     {name: '鲸山觐海', value: 3800},
-];
+]; // 房价总价前十的房子的价格。
 
+// 将从后端渲染来的数据进行格式转换，为后续在 bmap 上绘制散点图做准备。
 const convertData = function (data, label_data) {   //转换数据
     label_data = label_data['total_price_top10'];
     let res = [];
@@ -54,7 +63,8 @@ const convertData = function (data, label_data) {   //转换数据
     return res;
 };
 
-var clearData = function (data) {
+// 将从后端渲染来的数据进行格式转换，为后续在 bmap 上绘制热力图做准备。
+var clearData = function (data) { // 参考了 echarts 官网的热力图示例中的功能函数。
     var points = [].concat.apply(
         [],
         data.map(function (track) {
@@ -66,76 +76,77 @@ var clearData = function (data) {
     return points;
 };
 
-var t_data = clearData(heat_map_data_json);
-myChart.setOption(
+var t_data = clearData(heat_map_data_json);// 数据转换
+
+myChart.setOption( // echarts 设置选项
     (option = {
         tooltip: {
             trigger: 'item',
             formatter: function (val) {
-                let val_str = "房屋名字:" + val['data']['value'][2]['house_name'] +
-                    "<br/>户型:" + val['data']['value'][2]['house_type'] + "<br/>" +
-                    "总面积:" + val['data']['value'][2]['house_area'] + "平方米<br/>" +
-                    "总价:" + val['data']['value'][2]['total_price'] + "万";
-                return val_str;//只支持h5形式的显示
-            },
+                let val_str = "房屋名字: " + val['data']['value'][2]['house_name'] +
+                    "<br/>户型: " + val['data']['value'][2]['house_type'] + "<br/>" +
+                    "总面积: " + val['data']['value'][2]['house_area'] + " 平方米<br/>" +
+                    "总价: " + val['data']['value'][2]['total_price'] + " 万";
+                return val_str; // 只支持 h5 形式的显示
+            }, // 自定义提示框内容。
         },
-        animation: false,
+        animation: false, // 关闭动画效果
         bmap: {
-            center: [114.063836, 22.543884],  // 福田站坐标
-            zoom: 12,
-            roam: true
+            center: [114.063836, 22.543884],  // 设置百度地图的中心，位于福田站
+            zoom: 12, // 地图初始化缩放等级为 12
+            roam: true // 允许地图使用滚轮进行缩放
         },
         visualMap: {
-            show: false,
-            top: 'top',
-            min: 0,
-            max: 5,
-            seriesIndex: 2,
-            calculable: true,
-            inRange: {
+            show: false, // 是否显示 visualMap-continuous 组件。如果设置为 false，不会显示，但是数据映射的功能还存在
+            top: 'top', //组件离容器上侧的距离,'top', 'middle', 'bottom','20%
+            min: 0, // 指定 visualMapContinuous 组件的允许的最小值
+            max: 5, // 指定 visualMapContinuous 组件的允许的最大值
+            seriesIndex: 2, // 指定取哪个系列的数据，即哪个系列的 series.data ,这里指定的是 heatmap
+            calculable: true, // 是否显示拖拽用的手柄（手柄能拖拽调整选中范围）
+            inRange: { // 定义在选中范围中的视觉元素的颜色
                 color: ['blue', 'blue', 'green', 'yellow', 'red']
             }
         },
         series: [
             {
-                type: 'scatter',
-                coordinateSystem: 'bmap',
+                type: 'scatter', // 散点图
+                coordinateSystem: 'bmap', // 坐标系为百度地图坐标系
                 data: convertData(total_price, top10_data_json),
                 pointSize: 5,
                 symbolSize: function (val) {
                     return (val[2]['total_price'] / 30) / 10;
                 },
                 label: {
-                    normal: {   //默认
+                    normal: {   // 默认
                         formatter: '{b}',
                         position: 'right',
                         show: false
                     },
-                    emphasis: {  //悬浮
+                    emphasis: {  // 悬浮
                         show: true
                     }
                 },
                 itemStyle: {
                     normal: {
-                        color: 'rgba(255,0,0,0.6)',   //小圆点的颜色
+                        color: 'rgba(255,0,0,0.6)',   // 小圆点的颜色
                     }
                 }
-            },//上面是固定大小远点，往下是涟漪点
+            },// 上面是固定大小远点，往下是涟漪点
             {
                 name: 'Top 10',
-                type: 'effectScatter',  //带有涟漪特效动画的散点（气泡）
-                coordinateSystem: 'bmap',  //坐标系统
+                type: 'effectScatter',  // 带有涟漪特效动画的散点（气泡）
+                coordinateSystem: 'bmap',  // 坐标系统
                 data: convertData(total_price.sort(function (a, b) {  //数据排序，截取前10个
                     return b.value - a.value;
                 }).slice(0, 10), top10_data_json),
-                symbolSize: function (val) {   //标志图形大小
-                    return (val[2]['total_price'] / 30) / 10;//涟漪圈大小
+                symbolSize: function (val) {   // 标志图形大小
+                    return (val[2]['total_price'] / 30) / 10;// 涟漪圈大小
                 },
-                showEffectOn: 'render',   //绘制完成后显示特效。
-                rippleEffect: {           //涟漪特效相关配置。
-                    brushType: 'stroke'    //波纹的绘制方式
+                showEffectOn: 'render',   // 绘制完成后显示特效。
+                rippleEffect: {           // 涟漪特效相关配置。
+                    brushType: 'stroke'    // 波纹的绘制方式
                 },
-                hoverAnimation: true,
+                hoverAnimation: true, // 鼠标移入到图上不放大但会将内容显示在中心.
                 label: {
                     normal: {
                         formatter: '{b}',
@@ -146,16 +157,16 @@ myChart.setOption(
                 itemStyle: {
                     normal: {
                         color: 'rgba(255,0,0,0.6)',
-                        shadowBlur: 0.8,     //模糊度
+                        shadowBlur: 0.8,     // 模糊度
 
-                        // shadowColor: '#333'  //阴影颜色
+                        // shadowColor: '#333'  // 阴影颜色
                     }
                 },
                 zlevel: 1      // 一级层叠控制
             },
             {
-                type: 'heatmap',
-                coordinateSystem: 'bmap',
+                type: 'heatmap', // 热力图
+                coordinateSystem: 'bmap', // 坐标系 百度地图
                 data: t_data,
                 pointSize: 5,
                 blurSize: 6,
@@ -182,11 +193,14 @@ const Bar_option = {
     series: [{type: 'bar'}]
 };*/
 
-// 添加百度地图插件
-let bmap = myChart.getModel().getComponent('bmap').getBMap();
-bmap.addControl(new BMap.MapTypeControl());
+
+let bmap = myChart.getModel().getComponent('bmap').getBMap(); // 百度地图实例初始化
+
+bmap.addControl(new BMap.MapTypeControl()); // 添加百度地图插件
+
 if (option && typeof option === 'object') {
-    myChart.setOption(option);
+    myChart.setOption(option); // 如果还没绘图就开始绘图，如果已经绘图就不再绘图，避免重复加载。
     // myBarChart.setOption(Bar_option);
 }
-window.addEventListener('resize', myChart.resize);
+
+window.addEventListener('resize', myChart.resize); // 让echarts图表随着浏览器窗口的大小变化而变化
